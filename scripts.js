0508ac4bce13505e6091cdaebbbec12834b01a3b
Noxik <64279796+Noxik@users.noreply.github.com>
2023-04-15 15:23:29 +0200

    add extra input where user can define size of the board; add JS to change board size after Submit input value + also remove old board because without it we added new smalldiv to old board and didnt create new

--- scripts.js
+++ scripts.js
@@ -1,18 +1,50 @@
 let container = document.querySelector(".container");
+let sizeLength = "16"; /* default board size value */
+window.onload = createGrid() /* create default board after page load*/
 
-let sizeLength = 16;
-let columns = Math.pow(sizeLength, 2);
+/* size from input */
+const submitBtn = document.getElementById("submit")
 
-for(let i=0; i<columns; i++) {
+/* create new board from input value */
+submitBtn.addEventListener("click", () => {
+    sizeLength = document.getElementById('number').value;
+    if (sizeLength > 64) {
+        alert("board size is too big - your computer will explode!")
+    } else {
+    /* change grid style - important to create square board */
+    container.style.cssText = `grid-template-columns: repeat(${sizeLength}, 1fr)`
+
+    /* remove previous board */
+    while (container.firstChild) {
+        container.firstChild.remove()
+    } 
+
+    /* create new board with submit range */
+    createGrid();
+    }
+});
+
+
+/* create grid with mouse "hover effect*/
+function createGrid() {
+    let columns = Math.pow(sizeLength, 2);
+    for(let i=0; i<columns; i++) {
 const smalldiv = document.createElement("div");
 smalldiv.classList.add("smalldiv")
 container.appendChild(smalldiv);
 }
 
+/* "hover" effect */
 const smalldiv = document.querySelectorAll(".smalldiv");
-
 smalldiv.forEach( function(e){
 e.addEventListener("mousemove", () => {
     e.style.backgroundColor = "red"
   });
 })
+}
+
+
+
+
+
+
