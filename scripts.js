c7e23d7e720621b01748efd099dcfaab600a0f8e
Noxik <64279796+Noxik@users.noreply.github.com>
2023-04-16 15:19:31 +0200

    added Default color button; add Darkness mode which change brightness 1 -> 0.5, need upgdate to 10% reduction per move

--- scripts.js
+++ scripts.js
@@ -32,19 +32,34 @@ clearBtn.addEventListener("click", () => {
     } 
     /* create new board with submit range */
     createGrid();
+   
     }
 );
 
+
 /* create grid with mouse "hover effect*/
 function createGrid() {
+let color = "blue"
 
 /* default color or random */
 let rainbow = false;
-console.log(rainbow)
 const random = document.getElementById("random");
 random.addEventListener("click", () => {
     rainbow = true;
-    console.log(rainbow)
+    darkmode = false;
+    console.log("dark: " + darkmode);
+    console.log("rain: " + rainbow)
+    }
+); 
+
+/* default color */
+const defaultColor = document.getElementById("default");
+defaultColor.addEventListener("click", () => {
+    rainbow = false;
+    darkmode = false;
+    color = "blue"
+    console.log("dark: " + darkmode);
+    console.log("rain: " + rainbow);
     }
 ); 
 
@@ -52,26 +67,51 @@ random.addEventListener("click", () => {
 let columns = Math.pow(sizeLength, 2);
     for(let i=0; i<columns; i++) {
 const smalldiv = document.createElement("div");
-smalldiv.classList.add("smalldiv")
+smalldiv.classList.add("smalldiv");
+smalldiv.style.backgroundColor = "white";
+smalldiv.style.filter = "brightness(1)"
 container.appendChild(smalldiv);
 }
 
 /* "hover" effect */
 const smalldiv = document.querySelectorAll(".smalldiv");
 smalldiv.forEach( function(e){
-e.addEventListener("mousemove", () => {
-    if (rainbow == false) {
-    e.style.backgroundColor = "red"
-} else {
+e.addEventListener("mouseenter", () => {
+
+    if (rainbow == false  && darkmode == false) {
+    e.style.backgroundColor = color;
+   
+
+ } else if (darkmode == true) {
+    
+    let currentStyle = e.style.filter;
+    console.log(currentStyle)
+    if (currentStyle == "brightness(1)") {
+        e.style.filter = "brightness(0.5)"
+    }
+
+} else if (darkmode == false && rainbow == true) {
     var randomColor = Math.floor(Math.random()*16777215).toString(16);
     e.style.backgroundColor = "#" + randomColor;
+    e.style.filter = "brightness(1)"
 } 
   });
 })
 }
 
+let darkmode = false;
+const dark = document.getElementById("dark");
+dark.addEventListener("click", () => {
+    darkmode = true;
+    console.log("dark: " + darkmode);
+    rainbow = false
+    console.log("rain: " + rainbow)
+    })
 
-
-
-
-
+/* RANDOM RGB 
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+  }
+*/
